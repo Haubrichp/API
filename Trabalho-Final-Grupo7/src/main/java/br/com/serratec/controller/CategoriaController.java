02827af1f682f4002c406b6c665c633bfdb1a5b6
Haubrichp <162242102+Haubrichp@users.noreply.github.com>
2024-05-23 17:22:24 -0300

    Atualização Categorias

--- Trabalho-Final-Grupo7/src/main/java/br/com/serratec/controller/CategoriaController.java
+++ Trabalho-Final-Grupo7/src/main/java/br/com/serratec/controller/CategoriaController.java
@@ -1,16 +1,20 @@
 package br.com.serratec.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.serratec.entity.Categoria;
 import br.com.serratec.service.CategoriaService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/categorias")
@@ -24,6 +28,28 @@ public class CategoriaController {
 //	Pageable pageable) {
 //		
 //		return service.listarPorPagina(pageable);
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Categoria inserir(@Valid @RequestBody Categoria categoria) {
+		return service.inserir(categoria);
 	}
 	
+	@PutMapping("{id}")
+	public ResponseEntity<Categoria> atualizar(Long id ,@RequestBody Categoria categoria) {
+		if (service.equals(id)) {
+	        service.atualizar(id, categoria);
+	        return service.atualizar(id, categoria);
+	    }
+	    return ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        service.deletar(id);
+    }
+
+    
+	}
+
 
